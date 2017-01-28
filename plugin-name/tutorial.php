8b01943deeab5c2826c762ac252bce6047f5b45c
Joe <ujbeszel@gmail.com>
2017-01-28 19:02:27 +0100

    Fix missed shortcode functions

--- plugin-name/tutorial.php
+++ plugin-name/tutorial.php
@@ -594,6 +594,47 @@ if ( ! defined( 'WPINC' ) ) die;
  ********************************/
 
 ////////////////////////////////////////////////
+// ADD TO FILE -> includes/class-plugin-name-loader.php
+
+/**
+ * The array of shortcodes registered with WordPress.
+ *
+ * @since    1.0.0
+ * @access   protected
+ * @var      array    $shortcodes    The shortcodes registered with WordPress to fire when the plugin loads.
+ */
+protected $shortcodes;
+
+    /**
+     * Initialize the collections used to maintain the actions and filters.
+     *
+     * @since    1.0.0
+     */
+    public function __construct() {
+
+        // ...
+
+        $this->shortcodes = array();
+
+    }
+
+/**
+ * Add a new shortcode to the collection to be registered with WordPress
+ *
+ * @since     1.0.0
+ * @param     string        $tag           The name of the new shortcode.
+ * @param     object        $component      A reference to the instance of the object on which the shortcode is defined.
+ * @param     string        $callback       The name of the function that defines the shortcode.
+ */
+public function add_shortcode( $tag, $component, $callback, $priority = 10, $accepted_args = 2 ) {
+    $this->shortcodes = $this->add( $this->shortcodes, $tag, $component, $callback, $priority, $accepted_args );
+}
+
+foreach ( $this->shortcodes as $hook ) {
+    add_shortcode(  $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+}
+
+////////////////////////////////////////////////
 // ADD TO FILE -> includes/class-plugin-name.php
 
 private function define_public_hooks() {
