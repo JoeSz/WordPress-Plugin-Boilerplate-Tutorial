78a8e0bd873a5962494469ba8ad7ffab4e8b2622
Joseph VanTine <yobdmail@gmail.com>
2018-08-22 11:42:47 +0100

    fixed mixed example fields
    - The three example fields where mixed. $checkbox was called radio in some placeses and $select was called $checkbox in some places
    - There was an unnecessary ?> tag and no closing .wrap tag

--- plugin-name/tutorials/app_option_page_for_plugin.php
+++ plugin-name/tutorials/app_option_page_for_plugin.php
@@ -113,19 +113,19 @@ if ( ! defined( 'WPINC' ) ) die;
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <div class="wrap">
-    <h2>Plugin name <?php _e(' Options', $this->plugin_name); ?></h2>
+    <h2>Plugin Name <?php _e('Options', $this->plugin_name); ?></h2>
+
     <form method="post" name="cleanup_options" action="options.php">
     <?php
         //Grab all options
-        $options = get_option($this->plugin_name);
-
-        $example_select = ( isset( $options['example_checkbox'] ) && ! empty( $options['example_checkbox'] ) ) ? esc_attr( $options['example_checkbox'] ) : '1';
+        $options = get_option($this->plugin_name); 
+        
+        $example_select = ( isset( $options['example_select'] ) && ! empty( $options['example_select'] ) ) ? esc_attr( $options['example_select'] ) : '1';
         $example_text = ( isset( $options['example_text'] ) && ! empty( $options['example_text'] ) ) ? esc_attr( $options['example_text'] ) : 'default';
-        $example_radio = ( isset( $options['example_radio'] ) && ! empty( $options['example_radio'] ) ) ? 1 : 0;
-
+        $example_checkbox = ( isset( $options['example_checkbox'] ) && ! empty( $options['example_checkbox'] ) ) ? 1 : 0;
+        
         settings_fields($this->plugin_name);
         do_settings_sections($this->plugin_name);
-
         // Sources: - http://searchengineland.com/tested-googlebot-crawls-javascript-heres-learned-220157
         //          - http://dinbror.dk/blog/lazy-load-images-seo-problem/
         //          - https://webmasters.googleblog.com/2015/10/deprecating-our-ajax-crawling-scheme.html
@@ -156,17 +156,16 @@ if ( ! defined( 'WPINC' ) ) die;
 
     <!-- Checkbox -->
     <fieldset>
-        <p><?php _e( 'Example Radio.', $this->plugin_name ); ?></p>
-        <legend class="example-radio">
-            <span><?php _e( 'Example Radio', $this->plugin_name ); ?></span>
+        <p><?php _e( 'Example Checkbox.', $this->plugin_name ); ?></p>
+        <legend class="example-Checkbox">
+            <span><?php _e( 'Example Checkbox', $this->plugin_name ); ?></span>
         </legend>
-        <label for="<?php echo $this->plugin_name; ?>-example_radio">
-            <input type="checkbox" id="<?php echo $this->plugin_name; ?>-example_radio" name="<?php echo $this->plugin_name; ?>[example_radio]" value="1" <?php checked( $example_radio, 1 ); ?> />
-            <span><?php esc_attr_e('Example Radio', $this->plugin_name); ?></span>
+        <label for="<?php echo $this->plugin_name; ?>-example_checkbox">
+            <input type="checkbox" id="<?php echo $this->plugin_name; ?>-example_checkbox" name="<?php echo $this->plugin_name; ?>[example_checkbox]" value="1" <?php checked( $example_checkbox, 1 ); ?> />
+            <span><?php esc_attr_e('Example Checkbox', $this->plugin_name); ?></span>
         </label>
     </fieldset>
 
     <?php submit_button( __( 'Save all changes', $this->plugin_name ), 'primary','submit', TRUE ); ?>
     </form>
-
-?>
+</div>
