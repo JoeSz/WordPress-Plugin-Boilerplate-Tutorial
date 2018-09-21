6543a37b519e585a1554a84b69b96ff52a262b0f
Joe <ujbeszel@gmail.com>
2018-09-21 15:51:19 +0200

    Add Plugin Update Checker

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -30,6 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+define( 'PLUGIN_NAME_PLUGIN_NAME', 'plugin-name' );
 define( 'PLUGIN_NAME_BASE_DIR', plugin_dir_path( __FILE__ ) );
 
 /**
@@ -59,6 +60,69 @@ function deactivate_plugin_name() {
 
 register_activation_hook( __FILE__, 'activate_plugin_name' );
 register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+
+/*
+ * Update
+ */
+if ( is_admin() ) {
+
+	/**
+	 * A custom update checker for WordPress plugins.
+	 *
+	 * How to use:
+	 * - Copy vendor/plugin-update-checker to your plugin OR
+	 *   Download https://github.com/YahnisElsts/plugin-update-checker to the folder
+	 * - Create a subdomain or a folder for the update server eg. https://updates.example.net
+	 *   Download https://github.com/YahnisElsts/wp-update-server and copy to the subdomain or folder
+	 * - Add plguin zip to the 'packages' folder
+	 *
+	 * Useful if you don't want to host your project
+	 * in the official WP repository, but would still like it to support automatic updates.
+	 * Despite the name, it also works with themes.
+	 *
+	 * @link http://w-shadow.com/blog/2011/06/02/automatic-updates-for-commercial-themes/
+	 * @link https://github.com/YahnisElsts/plugin-update-checker
+	 * @link https://github.com/YahnisElsts/wp-update-server
+	 */
+	if( ! class_exists( 'Puc_v4_Factory' ) ) {
+
+		require_once join( DIRECTORY_SEPARATOR, array( PLUGIN_NAME_BASE_DIR, 'vendor', 'plugin-update-checker', 'plugin-update-checker.php' ) );
+
+	}
+
+	$MyUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+		'https://updates.example.net/?action=get_metadata&slug=' . PLUGIN_NAME_PLUGIN_NAME, //Metadata URL.
+		__FILE__, //Full path to the main plugin file.
+		PLUGIN_NAME_PLUGIN_NAME //Plugin slug. Usually it's the same as the name of the directory.
+	);
+
+	/**
+	 * add plugin upgrade notification
+	 * https://andidittrich.de/2015/05/howto-upgrade-notice-for-wordpress-plugins.html
+	 */
+	add_action( 'in_plugin_update_message-' . PLUGIN_NAME_PLUGIN_NAME . '/' . PLUGIN_NAME_PLUGIN_NAME .'.php', 'plugin_name_show_upgrade_notification', 10, 2 );
+	function plugin_name_show_upgrade_notification( $current_plugin_metadata, $new_plugin_metadata ) {
+
+		/**
+		 * Check "upgrade_notice" in readme.txt.
+		 *
+		 * Eg.:
+		 * == Upgrade Notice ==
+		 * = 20180624 = <- new version
+		 * Notice		<- message
+		 *
+		 */
+		if ( isset( $new_plugin_metadata->upgrade_notice ) && strlen( trim( $new_plugin_metadata->upgrade_notice ) ) > 0 ) {
+
+			// Display "upgrade_notice".
+			echo sprintf( '<div style="background-color:#d54e21;padding:10px;color:#f9f9f9;margin-top:10px"><strong>%1$s: </strong>%2$s</div>', esc_attr( 'Important Upgrade Notice', 'terminplaner' ), esc_html( $new_plugin_metadata->upgrade_notice ) );
+
+		}
+	}
+
+
+}
+// End Update
 
 /**
  * The core plugin class that is used to define internationalization,
