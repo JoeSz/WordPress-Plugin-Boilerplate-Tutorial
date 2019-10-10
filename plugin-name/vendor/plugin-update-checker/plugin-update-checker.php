d66dc5d95336d2cb52b926cce194002df2b6894c
Joe <ujbeszel@gmail.com>
2019-10-10 06:32:43 +0200

    Update Plugin Update Checker to 4,7

--- plugin-name/vendor/plugin-update-checker/plugin-update-checker.php
+++ plugin-name/vendor/plugin-update-checker/plugin-update-checker.php
@@ -1,24 +1,34 @@
 <?php
 /**
- * Plugin Update Checker Library 4.4
+ * Plugin Update Checker Library 4.7
  * http://w-shadow.com/
- * 
- * Copyright 2017 Janis Elsts
+ *
+ * Copyright 2019 Janis Elsts
  * Released under the MIT license. See license.txt for details.
  */
 
-require dirname(__FILE__) . '/Puc/v4p4/Factory.php';
+require dirname(__FILE__) . '/Puc/v4p7/Factory.php';
 require dirname(__FILE__) . '/Puc/v4/Factory.php';
-require dirname(__FILE__) . '/Puc/v4p4/Autoloader.php';
-new Puc_v4p4_Autoloader();
+require dirname(__FILE__) . '/Puc/v4p7/Autoloader.php';
+new Puc_v4p7_Autoloader();
 
-//Register classes defined in this file with the factory.
-Puc_v4_Factory::addVersion('Plugin_UpdateChecker', 'Puc_v4p4_Plugin_UpdateChecker', '4.4');
-Puc_v4_Factory::addVersion('Theme_UpdateChecker', 'Puc_v4p4_Theme_UpdateChecker', '4.4');
+//Register classes defined in this version with the factory.
+foreach (
+	array(
+		'Plugin_UpdateChecker' => 'Puc_v4p7_Plugin_UpdateChecker',
+		'Theme_UpdateChecker'  => 'Puc_v4p7_Theme_UpdateChecker',
 
-Puc_v4_Factory::addVersion('Vcs_PluginUpdateChecker', 'Puc_v4p4_Vcs_PluginUpdateChecker', '4.4');
-Puc_v4_Factory::addVersion('Vcs_ThemeUpdateChecker', 'Puc_v4p4_Vcs_ThemeUpdateChecker', '4.4');
+		'Vcs_PluginUpdateChecker' => 'Puc_v4p7_Vcs_PluginUpdateChecker',
+		'Vcs_ThemeUpdateChecker'  => 'Puc_v4p7_Vcs_ThemeUpdateChecker',
 
-Puc_v4_Factory::addVersion('GitHubApi', 'Puc_v4p4_Vcs_GitHubApi', '4.4');
-Puc_v4_Factory::addVersion('BitBucketApi', 'Puc_v4p4_Vcs_BitBucketApi', '4.4');
-Puc_v4_Factory::addVersion('GitLabApi', 'Puc_v4p4_Vcs_GitLabApi', '4.4');
+		'GitHubApi'    => 'Puc_v4p7_Vcs_GitHubApi',
+		'BitBucketApi' => 'Puc_v4p7_Vcs_BitBucketApi',
+		'GitLabApi'    => 'Puc_v4p7_Vcs_GitLabApi',
+	)
+	as $pucGeneralClass => $pucVersionedClass
+) {
+	Puc_v4_Factory::addVersion($pucGeneralClass, $pucVersionedClass, '4.7');
+	//Also add it to the minor-version factory in case the major-version factory
+	//was already defined by another, older version of the update checker.
+	Puc_v4p7_Factory::addVersion($pucGeneralClass, $pucVersionedClass, '4.7');
+}
