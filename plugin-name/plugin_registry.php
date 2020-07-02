9b2928808986b2554d9ee62745cee5bb4e86b0e3
Joe <ujbeszel@gmail.com>
2020-07-02 08:45:05 +0200

    Update plugin_registry.php

--- plugin-name/plugin_registry.php
+++ plugin-name/plugin_registry.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Class to save plugin instances for later use.
+ *
+ * Idea: https://github.com/DevinVinson/WordPress-Plugin-Boilerplate/issues/217#issuecomment-55960466
+ * For "Tom McFarlin"case, it is integrated inside in the plugin, but in my case (for working with multiple plugins)
+ * it is better to use as a standalone class.
+ *
+ * How to use:
+ * - Set (with plugin boilerplate)
+ *   require_once( 'plugin_registry.php' );
+ *   $registry = Plugin_Registry::get_instance();
+ *   $registry->add( 'exopite_password_policy', new Exopite_Password_Policy() );
+ *   $registry->get( 'exopite_password_policy' )->run();
+ *
+ * - Get
+ *   $registry = Plugin_Registry::get_instance();
+ *   $my_plugin = $registry->get( 'my_plugin' );
+ */
 
 if ( ! class_exists( 'Plugin_Registry' ) ) :
 
